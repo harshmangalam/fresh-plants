5df009c3a2ea736624b4a57cd866a808a38b2f30
harshmangalam <harshdev8218@gmail.com>
2022-07-21 08:27:22 +0530

    added auth

---
+++ routes/auth/index.tsx
@@ -0,0 +1,13 @@
+/**@jsx h */
+
+import {h} from "preact"
+import {tw} from "twind"
+
+export default function AuthHome(){
+    return (
+        <div className={tw``}>
+            Auth Home
+        </div>
+    )
+}
+
