4a1829818e54c559d97fab9596962f09b52fcdf7
harshmangalam <harshdev8218@gmail.com>
2022-07-21 09:41:45 +0530

    added signup screen

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,13 +4,15 @@
 
 import * as $0 from "./routes/auth/index.tsx";
 import * as $1 from "./routes/auth/login.tsx";
-import * as $2 from "./routes/index.tsx";
+import * as $2 from "./routes/auth/signup.tsx";
+import * as $3 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/auth/index.tsx": $0,
     "./routes/auth/login.tsx": $1,
-    "./routes/index.tsx": $2,
+    "./routes/auth/signup.tsx": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ routes/auth/signup.tsx
@@ -0,0 +1,71 @@
+/**@jsx h */
+
+import { h } from "preact";
+import { tw } from "twind";
+import AuthLayout from "../../layouts/AuthLayout.tsx";
+
+export default function AuthSignup() {
+  return (
+    <AuthLayout>
+      <div className={tw`max-w-md w-full p-6 shadow-md bg-white rounded-lg`}>
+        {/* login card top section  */}
+        <section className={tw`flex flex-col space-y-2 items-center`}>
+          <div className={tw`flex items-center space-x-3`}>
+            <img className={tw`w-12 h-12`} src="/logo.svg" alt="Shop logo" />
+            <h2 className={tw`text-xl font-bold`}>Fresh Shop</h2>
+          </div>
+          <p className={tw`font-medium text-sm text-gray-500`}>
+            Already have an account ?{" "}
+            <a className={tw`font-medium text-blue-400`} href="/auth/login">
+              Login
+            </a>
+          </p>
+        </section>
+
+        {/* login card body section  */}
+
+        <form method="post" className={tw`flex flex-col space-y-4 mt-6`}>
+          <div className={tw`flex flex-col space-y-2`}>
+            <label className={tw`font-normal text-gray-600`} htmlFor="name">
+              Name
+            </label>
+            <input
+              className={tw`outline-none px-4 py-3 border border-gray-400 rounded-md focus:ring-2 focus:ring-blue-400`}
+              type="name"
+              name="name"
+              id="name"
+            />
+          </div>
+          <div className={tw`flex flex-col space-y-2`}>
+            <label className={tw`font-normal text-gray-600`} htmlFor="email">
+              Email
+            </label>
+            <input
+              className={tw`outline-none px-4 py-3 border border-gray-400 rounded-md focus:ring-2 focus:ring-blue-400`}
+              type="email"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className={tw`flex flex-col space-y-2`}>
+            <label className={tw`font-normal text-gray-600`} htmlFor="password">
+              Password
+            </label>
+            <input
+              className={tw`outline-none px-4 py-3 border border-gray-400 rounded-md focus:ring-2 focus:ring-blue-400`}
+              type="password"
+              name="password"
+              id="password"
+            />
+          </div>
+
+          <button
+            className={tw`bg-blue-400 text-lg font-bold text-white rounded-md w-full py-3 focus:outline-none focus:ring focus:ring-blue-500`}
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+}
