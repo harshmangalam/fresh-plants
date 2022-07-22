bfff8765cc403bc3bf8574ade58b1a2c9e38c223
harshmangalam <harshdev8218@gmail.com>
2022-07-22 10:00:07 +0530

    added user role

--- database/schemas/User.ts
+++ database/schemas/User.ts
@@ -1,10 +1,15 @@
 import { ObjectId } from "mongo";
 
+enum UserRole {
+  user,
+  admin,
+}
 export interface UserSchema {
   _id?: ObjectId;
   name: string;
   email: string;
   password?: string;
+  role: UserRole;
   createdAt: Date;
   updatedAt: Date;
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,14 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
-  return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-    Home
-    </div>
-  );
+export const handler: Handlers = {
+  GET(req,ctx){
+    console.log(ctx.state.user)
+    return ctx.render()
+  }
+};
+export default function Home({ data }: PageProps) {
+  return <div class={tw`p-4 mx-auto max-w-screen-md`}>Home</div>;
 }
