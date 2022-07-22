72efa27e6654ddecb0a5acfdd26e80441d845d9d
harshmangalam <harshdev8218@gmail.com>
2022-07-22 10:41:51 +0530

    added icons

---
+++ icons/GraphIcon.tsx
@@ -0,0 +1,23 @@
+/**@jsx h */
+
+import { h } from "preact";
+import { tw } from "twind";
+
+export default function GraphIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={tw`h-6 w-6`}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+      />
+    </svg>
+  );
+}
