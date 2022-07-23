a478efeffe99c509cd83f144261b5a8e19f575d0
harshmangalam <harshdev8218@gmail.com>
2022-07-23 22:27:23 +0530

    added notification icon

--- layouts/AdminLayout.tsx
+++ layouts/AdminLayout.tsx
@@ -7,6 +7,7 @@ import ProductIcon from "@icons/ProductIcon.tsx";
 import UserIcon from "@icons/UserIcon.tsx";
 import SettingIcon from "@icons/SettingIcon.tsx";
 import OrderIcon from "@icons/OrderIcon.tsx";
+import NotificationIcon from "@icons/Notificationicon.tsx";
 interface Props {
   children: ComponentChildren;
   pathname: string;
@@ -14,25 +15,27 @@ interface Props {
 export default function AdminLayout({ children, pathname }: Props) {
   return (
     <div>
-      {/* sidebar  */}
+      {/* dashboard sidebar */}
       <aside
         className={tw`absolute top-0 left-0 bottom-0 overflow-y-auto bg-white w-[20%]  p-6  shadow`}
       >
         <section className={tw`flex flex-col space-y-6`}>
-          {/* logo  */}
+          {/*  dashboard sidebar logo  */}
           <a href="/" className={tw`flex space-x-3 items-center`}>
             <img className={tw`w-10 h-10`} src="/logo.svg" alt="Shop logo" />
             <h1 className={tw`text-xl font-bold`}>Dashboard</h1>
           </a>
 
-          {/* menu items  */}
+          {/* dashboard sidebar menu links  */}
 
           <div className={tw`flex flex-col space-y-3`}>
             {menus.map((menu) => (
               <a
                 href={menu.href}
                 className={tw`${
-                  pathname === menu.href ? "text-gray-900 bg-gray-100" : "text-gray-500"
+                  pathname === menu.href
+                    ? "text-gray-900 bg-gray-100"
+                    : "text-gray-500"
                 } font-medium  hover:text-gray-900 flex items-center space-x-2 py-3 px-2 hover:bg-gray-100 rounded-md transition ease-in-out  duration-300`}
               >
                 <div>{menu.icon}</div>
@@ -42,24 +45,14 @@ export default function AdminLayout({ children, pathname }: Props) {
           </div>
         </section>
       </aside>
+
+      {/* dashboard main content  */}
       <main className={tw`ml-[20%] h-screen overflow-y-auto pb-4`}>
+        {/* dashboard navbar  */}
         <nav className={tw`flex justify-end items-center p-6 `}>
           <div className={tw`flex space-x-6 items-center `}>
             <div className={tw`relative`}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={tw`w-8 h-8`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
+              <NotificationIcon />
 
               <span
                 className={tw`animate-bounce absolute w-2 h-2 rounded-full bg-red-500 top-2 right-1`}
