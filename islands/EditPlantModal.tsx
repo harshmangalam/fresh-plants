996971bbb28d7b1d443b08a40b45850729c03f79
harshmangalam <harshdev8218@gmail.com>
2022-07-23 19:31:36 +0530

    change ui color

--- islands/EditPlantModal.tsx
+++ islands/EditPlantModal.tsx
@@ -190,7 +190,7 @@ export default function EditPlantModal({
                   >
                     <button
                       type="submit"
-                      className={tw`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm`}
+                      className={tw`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-400 text-base font-medium text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600 sm:ml-3 sm:w-auto sm:text-sm`}
                       name="_action"
                       value="edit"
                     >
