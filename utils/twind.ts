1a3345ab19b56f0ed62ce8de41b57a5be6071613
harshmangalam <harshdev8218@gmail.com>
2022-07-23 12:31:12 +0530

    added forms plugin

--- utils/twind.ts
+++ utils/twind.ts
@@ -1,8 +1,12 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
+import {forms}  from "https://esm.sh/@twind/forms@0.1.4"
 export * from "twind";
 export const config: Configuration = {
   darkMode: "class",
   mode: "silent",
+  plugins:{
+    forms
+  }
 };
 if (IS_BROWSER) setup(config);
