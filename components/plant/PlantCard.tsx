fdfea60ddb1b791676bac7b4cf99271ef499d00a
harshmangalam <harshdev8218@gmail.com>
2022-07-24 13:46:04 +0530

    card link

--- components/plant/PlantCard.tsx
+++ components/plant/PlantCard.tsx
@@ -23,11 +23,13 @@ export default function PlantCard({
 }: Props) {
   return (
     <article className={tw`bg-white border shadow rounded-lg `}>
-      <img
-        src={showFile(image)}
-        alt={name}
-        className={tw`w-full h-60 object-cover`}
-      />
+      <a href={`/${_id}`}>
+        <img
+          src={showFile(image)}
+          alt={name}
+          className={tw`w-full h-60 object-cover`}
+        />
+      </a>
       <div className={tw`px-2 py-4`}>
         <div className={tw`flex items-center justify-between`}>
           <p
@@ -44,9 +46,16 @@ export default function PlantCard({
             {quantity} left
           </p>
         </div>
-
-        <h3 className={tw`truncate text-gray-600 text-lg font-medium`}>{name}</h3>
-        <button className={tw`mt-4 bg-blue-400 text-white font-bold w-full px-4 py-2 rounded-md focus:outline-none hover:bg-blue-500`}>Add to cart</button>
+        <a href={`/${_id}`}>
+          <h3 className={tw`truncate text-gray-600 text-lg font-medium`}>
+            {name}
+          </h3>
+        </a>
+        <button
+          className={tw`mt-4 bg-blue-400 text-white font-bold w-full px-4 py-2 rounded-md focus:outline-none hover:bg-blue-500`}
+        >
+          Add to cart
+        </button>
       </div>
     </article>
   );
