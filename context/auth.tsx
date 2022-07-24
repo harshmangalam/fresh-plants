cc58460a0d96b2707f5d69eeaab199ee632b0274
harshmangalam <harshdev8218@gmail.com>
2022-07-24 16:00:26 +0530

    clear folder

--- context/auth.tsx
+++
@@ -1,8 +0,0 @@
-/** @jsx h */
-
-import { useContext } from "preact/hooks";
-import { createContext } from "preact";
-
-export const AuthContext = createContext(null);
-
-export const useAuth = () => useContext(AuthContext);
