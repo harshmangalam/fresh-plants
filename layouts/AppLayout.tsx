05d2a10995574d3fd224d3b7587402645ddb7140
harshmangalam <harshdev8218@gmail.com>
2022-07-24 13:57:49 +0530

    added navbar

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -3,6 +3,7 @@
 import { Head } from "$fresh/runtime.ts";
 import { ComponentChildren, h } from "preact";
 import { tw } from "twind";
+import CartIcon from "@icons/CartIcon.tsx"
 
 interface Props {
   children: ComponentChildren;
@@ -15,11 +16,19 @@ export default function AppLayout({ children, title }: Props) {
         <title>{title}</title>
       </Head>
       <nav className={tw`bg-white`}>
-        <div className={tw`flex items-center justify-between h-16 max-w-6xl mx-auto`}>
+        <div
+          className={tw`flex items-center justify-between h-16 max-w-6xl mx-auto`}
+        >
           <a href="/" className={tw`flex space-x-3 items-center`}>
             <img className={tw`w-10 h-10`} src="/logo.svg" alt="Shop logo" />
             <h1 className={tw`text-2xl font-medium`}>Fresh Plants</h1>
           </a>
+
+          <div>
+            <a href="/cart" className={tw``}>
+              <CartIcon />
+            </a>
+          </div>
         </div>
       </nav>
       <main className={tw`py-4 px-4 max-w-6xl mx-auto`}>{children}</main>
